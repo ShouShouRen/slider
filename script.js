ced0e48687bfd8054ec5407fafecfa44b22acf14
Peter Chen <peterchen2017125@gmail.com>
2023-04-24 21:10:18 +0800

    20230424

--- script.js
+++ script.js
@@ -1,11 +1,31 @@
-document.getElementById('next').onclick = function () {
-  let lists = document.querySelectorAll('.item');
-  document.getElementById('slide').appendChild(lists[0]);
-}
-document.getElementById('prev').onclick = function () {
-  let lists = document.querySelectorAll('.item');
-  document.getElementById('slide').prepend(lists[lists.length - 1]);
-}
+// document.getElementById('next').onclick = function () {
+//   let lists = document.querySelectorAll('.item');
+//   document.getElementById('slide').appendChild(lists[0]);
+// }
+// document.getElementById('prev').onclick = function () {
+//   let lists = document.querySelectorAll('.item');
+//   document.getElementById('slide').prepend(lists[lists.length - 1]);
+// }
+
+// let autoPlayInterval;
+
+// function startAutoPlay() {
+//   autoPlayInterval = setInterval(function () {
+//     let lists = document.querySelectorAll('.item');
+//     document.getElementById('slide').appendChild(lists[0]);
+//   }, 4500);
+// }
+
+// startAutoPlay();
+
+// function stopAutoPlay() {
+//   clearInterval(autoPlayInterval);
+// }
+
+
+// // stopAutoPlay();
+
+
 
 let autoPlayInterval;
 
@@ -16,11 +36,26 @@ function startAutoPlay() {
   }, 4500);
 }
 
-startAutoPlay();
-
 function stopAutoPlay() {
   clearInterval(autoPlayInterval);
 }
 
+document.getElementById('next').onclick = function () {
+  stopAutoPlay();
+  setTimeout(function () {
+    startAutoPlay();
+  }, 3000);
+  let lists = document.querySelectorAll('.item');
+  document.getElementById('slide').appendChild(lists[0]);
+};
 
-// stopAutoPlay();
+document.getElementById('prev').onclick = function () {
+  stopAutoPlay();
+  setTimeout(function () {
+    startAutoPlay();
+  }, 3000);
+  let lists = document.querySelectorAll('.item');
+  document.getElementById('slide').prepend(lists[lists.length - 1]);
+};
+
+startAutoPlay();
