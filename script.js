7254c12c73c48b57d72d907bee9b6d27a97eead9
Peter Chen <peterchen2017125@gmail.com>
2023-04-24 21:02:30 +0800

    20230424

--- script.js
+++ script.js
@@ -6,3 +6,21 @@ document.getElementById('prev').onclick = function () {
   let lists = document.querySelectorAll('.item');
   document.getElementById('slide').prepend(lists[lists.length - 1 ]);
 }
+
+let autoPlayInterval;
+
+function startAutoPlay() {
+  autoPlayInterval = setInterval(function() {
+    let lists = document.querySelectorAll('.item');
+    document.getElementById('slide').appendChild(lists[0]);
+  }, 4500); // 每隔3秒執行一次
+}
+
+startAutoPlay();
+
+function stopAutoPlay() {
+  clearInterval(autoPlayInterval);
+}
+
+
+// stopAutoPlay();
